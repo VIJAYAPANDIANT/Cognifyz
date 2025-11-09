dfcfe82e9f725b7c91398ae0759feac0d5090939
VIJAYAPANDIAN.T <vijayapandian112007@gmail.com>
2025-11-10 00:05:03 +0530

    cognifyz

--- L1task2.java
+++ L1task2.java
@@ -1,3 +1,21 @@
+
+import java.util.Scanner;
+
 public class L1task2 {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word or phrase: ");
+        String input = sc.nextLine();
+        String clean=input.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        String reversed = new StringBuilder(clean).reverse().toString();
+        if(clean.equals(reversed)){
+            System.out.println("The input is a palindrome.");
+        } else {
+            System.out.println("The input is not a palindrome.");
+        }
+        sc.close();
+
+    }
+    
     
 }
